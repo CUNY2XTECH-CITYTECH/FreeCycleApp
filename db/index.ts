619ad1834f47a9741db4aab32a4048555815c1e5
ZainAbidin38 <144952346+ZainAbidin38@users.noreply.github.com>
2025-02-05 14:51:01 -0500

    Added queries to create, read, update, and delete users

--- db/index.ts
+++ db/index.ts
@@ -1,4 +1,35 @@
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
+import { usersTable } from './schema';
+import { eq } from 'drizzle-orm';
 
 const db = drizzle(process.env.DATABASE_URL!);
+
+async function main() {
+    // Create a new user
+    const user: typeof usersTable.$inferInsert = {
+        name: 'John Doe',
+        email: 'johnDoe@example.com'
+    };
+    await db.insert(usersTable).values(user);
+    console.log('New USer Created');
+
+    // Get all users
+    const users = await db.select().from(usersTable);
+    console.log('Getting all users from the database', users)
+
+    //Update User
+    await db
+        .update(usersTable)
+        .set({ name: 'Jane Doe' })
+        .where(eq(usersTable.email, user.email));
+    console.log('User Info Updated');
+
+    // Delete User
+    await db
+        .delete(usersTable)
+        .where(eq(usersTable.email, user.email));
+    console.log('User Deleted');
+}
+
+main();
