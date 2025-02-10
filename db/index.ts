c170ba52fa1e3150efefc37dc2089dbb2c118d27
ZainAbidin38 <144952346+ZainAbidin38@users.noreply.github.com>
2025-02-10 16:31:29 -0500

    Created functions for users and some for products. Created a products, orders, and orders details table.

--- db/index.ts
+++ db/index.ts
@@ -1,35 +1,110 @@
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
-import { usersTable } from './schema';
+import { usersTable, productsTable } from './schema';
 import { eq } from 'drizzle-orm';
 
+
+// USERS
+
+// Connect to the database
+//Check if environment variable DATABASE_URL is set before connecting
+if (!process.env.DATABASE_URL) {
+    throw new Error('DATABASE_URL is not defined');
+}
 const db = drizzle(process.env.DATABASE_URL!);
 
-async function main() {
-    // Create a new user
-    const user: typeof usersTable.$inferInsert = {
-        name: 'John Doe',
-        email: 'johnDoe@example.com'
-    };
-    await db.insert(usersTable).values(user);
-    console.log('New USer Created');
+interface User {
+    name: string;
+    email: string;
+}
 
-    // Get all users
-    const users = await db.select().from(usersTable);
-    console.log('Getting all users from the database', users)
+async function createNewUser(user: User) {
+    await db
+    .insert(usersTable)
+    .values(user);
+    console.log('New User Created');
+}
 
+async function getUser(user_id: string) {
+    try {
+        const user = await db
+        .select()
+        .from(usersTable)
+        .where(eq(usersTable.User_id, user_id))
+        .limit(1); // Ensures only one user is returned
+        if (user.length === 0) {
+            console.log('User not found');
+            return null;
+        }
+        console.log('Getting user from the database', user)
+        return user;
+    } catch (error) {
+        console.log('Error getting users', error);
+    }
+}
+
+async function updateUser(email: string, newName: string) {
     //Update User
     await db
         .update(usersTable)
-        .set({ name: 'Jane Doe' })
-        .where(eq(usersTable.email, user.email));
+        .set({ name: newName })
+        .where(eq(usersTable.email, email));
     console.log('User Info Updated');
-
-    // Delete User
+}
+    
+async function deleteUser(email: string) {
+     // Delete User
     await db
         .delete(usersTable)
-        .where(eq(usersTable.email, user.email));
+        .where(eq(usersTable.email, email));
     console.log('User Deleted');
 }
 
-main();
+
+// PRODUCTS
+
+interface Product {
+    name: string;
+    description: string;
+    price: number;
+    stock: number;
+    category: string;
+    imageUrl: string;
+    created_at: Date;
+    updated_at: Date;
+    isAvailable: boolean;
+}
+
+async function createNewProduct(product: Product) {
+    await db
+    .insert(productsTable)
+    .values(product);
+    console.log('New Product Created');
+}
+
+async function getProduct(product_id: string) {
+    try {
+        const product = await db
+            .select()
+            .from(productsTable)
+            .where(eq(productsTable.name, product_id)) 
+            .limit(1); // Ensures only one product is returned
+            if (product.length === 0) {
+                console.log('Product not found');
+                return null;
+            }
+        console.log('Getting product from the database:', product);
+        return product;  // Returns the product data
+    } catch (error) {
+        console.log('Error getting product', error);
+    }
+}
+
+async function updateProduct(name: string, newPrice: number) {
+    //Update Product
+    await db
+        .update(productsTable)
+        .set({ price: newPrice })
+        .where(eq(productsTable.name, name));
+    console.log('Product Info Updated');
+}
--- db/schema.ts
+++ db/schema.ts
@@ -1,7 +1,41 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { timestamp } from "drizzle-orm/pg-core";
+import { boolean, integer, numeric, pgTable, text, uuid, varchar } from "drizzle-orm/pg-core";
+
 
  export const usersTable = pgTable ("users", {
-    id: integer().primaryKey().generatedAlwaysAsIdentity(),
-    name: varchar({ length: 255 }).notNull(),
-    email: varchar({ length: 255 }).notNull().unique(),
+    User_id: uuid().primaryKey().defaultRandom(),
+    name: varchar({ length: 50 }).notNull(),
+    email: varchar({ length: 100 }).notNull().unique(),
+});
+
+export const productsTable = pgTable("products", {
+    Product_id: uuid().primaryKey().defaultRandom(),
+    name: varchar({ length: 200 }).notNull(),
+    description: text().notNull(),
+    price: numeric().notNull().$type<number>(), // $type<number>() is used to convert number to string because drizzle expects price to be a string.
+    stock: integer().notNull(),
+    category: varchar({ length: 50 }).notNull(),
+    imageUrl: varchar({ length: 500 }).notNull(),
+    created_at: timestamp().defaultNow().notNull(),
+    updated_at: timestamp().defaultNow().notNull(),
+    isAvailable: boolean().default(true).notNull(),
+});
+
+export const ordersTable = pgTable("orders", {
+    order_id: uuid().primaryKey().defaultRandom(),
+    user_id: uuid().notNull().references(() => usersTable.User_id),
+    total_price: numeric().notNull(),
+    status: varchar({ length: 50 }).notNull().default('pending'),
+    created_at: timestamp().defaultNow().notNull(),
+    updated_at: timestamp().defaultNow().notNull(),
+    isPaid: boolean().default(false).notNull(),
+});
+
+export const orderDetailsTable = pgTable("order_details", {
+    order_detail_id: uuid().primaryKey().defaultRandom(),
+    order_id: uuid().notNull().references(() => ordersTable.order_id),
+    product_id: uuid().notNull().references(() => productsTable.Product_id),
+    quantity: integer().notNull().default(1),
+    price_at_purchase: numeric().notNull(),
+    subtotal: numeric().notNull(),
 });
