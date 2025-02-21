d60a236439ff46b8ee596af922ac2a882bdea645
ZainAbidin38 <144952346+ZainAbidin38@users.noreply.github.com>
2025-02-20 19:00:01 -0500

    initial commit, and created a route to get the posts from database.

---
+++ app/api/posts/route.ts
@@ -0,0 +1,21 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { Pool } from "pg";
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+});
+
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+    if (req.method === "GET") {
+      try {
+        const result = await pool.query('SELECT * FROM posts');
+        const posts = result.rows;
+        res.status(200).json(posts);
+      } catch (error) {
+        res.status(500).json({ message: "Internal Server Error" });
+      }
+    } else {
+      res.status(405).json({ message: "Method Not Allowed" });
+    }
+  };
