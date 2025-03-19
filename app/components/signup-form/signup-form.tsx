b273e3ac24b5a113528a81fc006084d03d6ee8b6
Carina Taveras <cee@Carinas-MacBook-Air.local>
2025-03-19 15:25:58 -0400

    added supertokens functionality to the signup-form.tsx

--- app/components/signup-form/signup-form.tsx
+++ app/components/signup-form/signup-form.tsx
@@ -1,6 +1,11 @@
 "use client";
 import { useState } from "react";
 
+import {
+  signUp,
+  doesEmailExist,
+} from "supertokens-web-js/recipe/emailpassword";
+
 export default function SignUpForm() {
   const [formData, setFormData] = useState({
     name: "",
@@ -12,9 +17,89 @@ export default function SignUpForm() {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const signUpClicked = async (email: string, password: string) => {
+    try {
+      const response = await signUp({
+        formFields: [
+          {
+            id: "email",
+            value: email,
+          },
+          {
+            id: "password",
+            value: password,
+          },
+        ],
+      });
+      console.log("Inside sign up clicked");
+      console.log(response.status + " Response status");
+
+      if (response.status === "FIELD_ERROR") {
+        // one of the input formFields failed validation
+        response.formFields.forEach((formField) => {
+          if (formField.id === "email") {
+            // Email validation failed (for example incorrect email syntax),
+            // or the email is not unique.
+            window.alert(formField.error);
+          } else if (formField.id === "password") {
+            // Password validation failed.
+            // Maybe it didn't match the password strength
+            window.alert(formField.error);
+          }
+        });
+      } else if (response.status === "SIGN_UP_NOT_ALLOWED") {
+        // the reason string is a user friendly message
+        // about what went wrong. It can also contain a support code which users
+        // can tell you so you know why their sign up was not allowed.
+        window.alert(response.reason);
+      } else {
+        // sign up successful. The session tokens are automatically handled by
+        // the frontend SDK.
+        window.location.href = "/home";
+      }
+    } catch (err: any) {
+      if (err.isSuperTokensGeneralError === true) {
+        // this may be a custom error message sent from the API by you.
+        window.alert(err.message);
+      } else {
+        window.alert("Oops! Something went wrong.");
+      }
+    }
+  };
+
+  const checkEmail = async (email: string) => {
+    try {
+      const response = await doesEmailExist({
+        email,
+      });
+
+      if (response.doesExist) {
+        window.alert("Email already exists. Please sign in instead");
+      }
+    } catch (err: any) {
+      if (err.isSuperTokensGeneralError === true) {
+        // this may be a custom error message sent from the API by you.
+        window.alert(err.message);
+      } else {
+        window.alert("Oops! Something went wrong.");
+      }
+    }
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log("Form Submitted:", formData);
+
+    await checkEmail(formData.email)
+    await signUpClicked(formData.email, formData.password)
+    
+    // clears the form
+    setFormData({
+      name: "",
+      email: "",
+      password: ""
+    })
+
   };
 
   return (
